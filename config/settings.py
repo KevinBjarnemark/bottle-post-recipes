92a13c050c9636b2f5202113232943c4cd425724
Kevin <kevin.bjarnemark1@gmail.com>
2024-08-15 11:24:38 +0200

    Added domains in ALLOWED_HOSTS for production and development

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bottle-post-recipes-eb1abd9c13ee.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
