5e26cf5947d8951b4f695ec829da9ade04551e56
Kevin <kevin.bjarnemark1@gmail.com>
2024-08-21 00:09:16 +0200

    Integrated the django browser reload python package

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_browser_reload', # Hot-reloading
     'pages.home',
 ]
 
@@ -38,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware", # Hot-reloading
 ]
 
 ROOT_URLCONF = 'config.urls'
