7ff615c049173b97a7c063bd218730987a1a439d
Kevin <kevin.bjarnemark1@gmail.com>
2024-09-04 21:14:30 +0200

    Added a profile model in the users app and integrated it with user registration

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,2 +1,26 @@
 from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    vegan_mode = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    last_reviewed_at = models.DateTimeField(null=True, blank=True)
+    # Method for tracking/limiting reviews
+    def can_review(self):
+        if self.last_reviewed_at:
+            return timezone.now() >= self.last_reviewed_at + datetime.timedelta(hours=24)
+        return True
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile_and_settings(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
