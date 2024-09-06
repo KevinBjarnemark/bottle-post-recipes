a92df65006678fabe4133ea39fc6716dc9315b5e
Kevin <kevin.bjarnemark1@gmail.com>
2024-09-06 09:06:48 +0200

    Added logic for handling profile images and integrated it with the account button + styling

--- apps/users/views.py
+++ apps/users/views.py
@@ -6,18 +6,30 @@ from django.contrib.auth.signals import user_logged_in
 # Import all profile model
 from .models import Profile
 from django.dispatch import receiver
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreationForm(request.POST, request.FILES)
         if form.is_valid():
+            # TODO Form validation
             user = form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
+            # Handle the Profile object and associate the image
+            profile_image = request.FILES.get('image')
+            profile, created = Profile.objects.get_or_create(user=user)
+            if profile_image:
+                profile.image = profile_image
+            profile.save()
+            # Automatically opt in the user and redirect to home
             login(request, user)
-            # Redirect to a home page or another page after registration
+            messages.success(request, 'Your account has been created!')
             return redirect('home')
+        else:
+            print(form.errors)
+            messages.error(request, "Please correct the error(s) below.")
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
