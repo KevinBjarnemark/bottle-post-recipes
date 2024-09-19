9b013939fcff7065bc27bfa19ed2730bcf6da6a6
Kevin <kevin.bjarnemark1@gmail.com>
2024-09-19 17:38:07 +0200

    Transformed the instruction model to a text field and fixed dev/prod environment setup for image + finetuning

--- apps/pages/create_recipe/admin.py
+++ apps/pages/create_recipe/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, Comment, Instruction, Time, EstimatedPricePerMeal
+from .models import Recipe, Ingredient, Comment, Time, EstimatedPricePerMeal
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(Comment)
-admin.site.register(Instruction)
 admin.site.register(Time)
 admin.site.register(EstimatedPricePerMeal)
--- apps/pages/create_recipe/models.py
+++ apps/pages/create_recipe/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -7,17 +8,20 @@ class Recipe(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    image = CloudinaryField('image', blank=True, null=True)
+    if settings.DEBUG:
+        image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
+    else:
+        image = CloudinaryField('image', blank=True, null=True)
     description = models.TextField()
     likes = models.IntegerField(default=0)
     bottle_posted_count = models.IntegerField(default=0)
     in_ocean = models.BooleanField(default=True)
     public = models.BooleanField(default=True)
     contains_alcohol = models.BooleanField(default=False)
-    dairy = models.BooleanField(default=False)
+    contains_dairy = models.BooleanField(default=False)
     vegan = models.BooleanField(default=False)
     tags = models.CharField(max_length=255, null=True, blank=True)
-    image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
+    instructions = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     last_edited = models.DateTimeField(auto_now=True)
     # Method of incrementing bottle_posted_count
@@ -30,12 +34,6 @@ class Ingredient(models.Model):
     quantity = models.CharField(max_length=50)
     name = models.CharField(max_length=255)
     unit = models.CharField(max_length=20)
-
-class Instruction(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='instructions')
-    step_number = models.IntegerField()
-    content = models.TextField()
-    type = models.CharField(max_length=20, choices=[('text', 'Text'), ('ordered_list', 'Ordered List')])
 
 class Time(models.Model):
     recipe = models.OneToOneField(Recipe, on_delete=models.CASCADE, related_name='time')
@@ -56,4 +54,3 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-
