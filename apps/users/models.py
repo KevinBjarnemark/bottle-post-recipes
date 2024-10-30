f2aae5b4d29454ff0daa276875c7b30581bb869b
Kevin <kevin.bjarnemark1@gmail.com>
2024-10-30 19:42:51 +0100

    Added review_recipe_id as a new value in user profile model

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 import datetime
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 
 class Profile(models.Model):
@@ -17,14 +18,23 @@ class Profile(models.Model):
     else:
         image = CloudinaryField('image', blank=True, null=True)
     last_reviewed_at = models.DateTimeField(null=True, blank=True)
+    review_recipe_id = models.IntegerField(null=True, blank=True)
 
     # Method for tracking/limiting reviews
     def can_review(self):
-        if self.last_reviewed_at:
-            now = datetime.timezone.now()
-            last_reviewed_at = self.last_reviewed_at
-            return now >= last_reviewed_at + datetime.timedelta(hours=24)
-        return True
+        # If user haven't reviewed ever, return True
+        if self.last_reviewed_at is None:
+            return True
+        # Calculate hours if they have reviewed before
+        elif self.last_reviewed_at:
+            now = timezone.now()
+            # Calculate if a day has passed since their last review
+            a_day_has_passed = (
+                now >= self.last_reviewed_at + datetime.timedelta(hours=24)
+            )
+            return a_day_has_passed
+        # Prevent unknown conditions to pass through
+        return False
 
 
 @receiver(post_save, sender=User)
